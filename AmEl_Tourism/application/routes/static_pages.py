9b9155ecbd1cc2ae7b3d4fdddacbd53094ae7b8e
AmEl <amirelkanov0504@gmail.com>
2020-06-28 00:17:59 +0300

    Cosmetic fixes

--- AmEl_Tourism/application/routes/static_pages.py
+++ AmEl_Tourism/application/routes/static_pages.py
@@ -10,7 +10,8 @@ from flask import Blueprint, render_template
 
 from ..data.articles import Article, Category
 from ..extensions.const import YANDEX_API_KEY
-from ..extensions.img_tags import img_tag, group_img_tag, img_group_tag, img_group_closing_tag
+from ..extensions.img_tags import img_tag, group_img_tag, img_group_tag, \
+    img_group_closing_tag
 from ..extensions.lonlat_converting import lonlat_to_bounds
 from ..page_navigation.pagination import PageNavigation
 
@@ -39,59 +40,75 @@ def articles_by_category(category_id: int, page_id: int = 1):
     """
 
     places = Article.query.filter_by(article_category_id=category_id).all()
-    return PageNavigation(page_id, Category).add_page_navigation(places, False, category_id)
+    return PageNavigation(page_id, Category).add_page_navigation(places, False,
+                                                                 category_id)
 
 
 @main.route('/article/<int:article_id>')
 def article(article_id: int) -> str:
     """ Showing article
-    :param article_id: the article (or more precisely, the id) that the user is on
+    :param article_id: the article (or more precisely, the id)
+    that the user is on
     """
 
     article_info = Article.query.filter_by(id=article_id).first()
 
     # Creating "Related posts"
     list_of_articles_by_category: list = \
-        Article.query.filter(Article.article_category_id == article_info.article_category_id,
-                             Article.id != article_info.id).all()
+        Article.query.filter(
+            Article.article_category_id == article_info.article_category_id,
+            Article.id != article_info.id).all()
     shuffle(list_of_articles_by_category)
-    related_posts: list = list_of_articles_by_category[:3] if len(list_of_articles_by_category) >= 3 else []
+    related_posts: list = list_of_articles_by_category[:3] if len(
+        list_of_articles_by_category) >= 3 else []
 
-    rendered_page = BeautifulSoup(render_template('article.html',
-                                                  title=article_info.title, article_info=article_info,
-                                                  category_list=Category.query, api_key=YANDEX_API_KEY,
-                                                  bounds_of_place=lonlat_to_bounds(article_info.coords.split(', ')),
-                                                  related_posts=related_posts), 'lxml')
+    article_template = render_template('article.html',
+                                       title=article_info.title,
+                                       article_info=article_info,
+                                       category_list=Category.query,
+                                       api_key=YANDEX_API_KEY,
+                                       bounds_of_place=lonlat_to_bounds(
+                                           article_info.coords.split(', ')),
+                                       related_posts=related_posts),
+    rendered_page = BeautifulSoup(article_template, 'lxml')
 
-    article_images, article_text = article_info.article_imgs.split(', '), article_info.text
+    article_images, article_text = article_info.article_imgs.split(
+        ', '), article_info.text
 
-    # ----------------------- Replacing input [blocks] to <html> blocks -----------------------
+    # --------------- Replacing input [blocks] to <html> blocks ---------------
 
     for i, article_img in enumerate(article_images):
 
-        image_tag_st_ind, group_img_tag_ind = article_text.find(img_tag), article_text.find(group_img_tag)
+        image_tag_st_ind, group_img_tag_ind = article_text.find(
+            img_tag), article_text.find(group_img_tag)
 
         # Inserting single image
         if image_tag_st_ind != -1:
-            article_text = article_text[:image_tag_st_ind] + '<br><p class="centered-image post-format">' + \
-                           f'<img src="{article_img}" alt="" class="pinit"></p>' + \
+            article_text = article_text[
+                           :image_tag_st_ind] + \
+                           '<br><p class="centered-image post-format">' + \
+                           f'<img src="{article_img}" alt="" ' \
+                           f'class="pinit"></p>' + \
                            article_text[image_tag_st_ind + len(img_tag):]
 
         # Inserting group image
         elif group_img_tag_ind != -1:
             article_text = article_text[:group_img_tag_ind] + \
-                           f'<li class="blocks-gallery-item"><img src="{article_img}" alt=""></li>' + \
+                           f'<li class="blocks-gallery-item">' \
+                           f'<img src="{article_img}" alt=""></li>' + \
                            article_text[group_img_tag_ind + len(group_img_tag):]
 
         # Inserting image group tag
-        article_text: str = article_text.replace(img_group_tag,
-                                                 '<br><p><ul class="wp-block-gallery columns-4 is-cropped">'). \
+        article_text: str = article_text.replace(
+            img_group_tag,
+            '<br><p><ul class="wp-block-gallery columns-4 is-cropped">'). \
             replace(img_group_closing_tag, '</ul>')
 
-    # -----------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
     # Inserting formatted code into div
-    rendered_page.find("div", id="article_text").insert(0, BeautifulSoup(article_text, 'lxml'))
+    rendered_page.find("div", id="article_text").insert(0, BeautifulSoup(
+        article_text, 'lxml'))
     return str(rendered_page)
 
 
