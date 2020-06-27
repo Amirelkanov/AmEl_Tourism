644fe25a2df735cdfaca2c74dd351feee6c48c42
AmEl <amirelkanov0504@gmail.com>
2020-06-28 00:13:28 +0300

    Cosmetic fixes

--- AmEl_Tourism/application/extensions/lonlat_converting.py
+++ AmEl_Tourism/application/extensions/lonlat_converting.py
@@ -9,4 +9,5 @@ def lonlat_to_bounds(lonlat: list) -> str:
     :param lonlat: coords like "['12.345678', '12.345678']"
     """
     return str([[round(eval(f'{j} {sign} {lonlat_factor[i]}'), 6)
-                 for i, j in enumerate(lonlat)] for sign in ['-', '+']]).replace(', [', ',[')
+                 for i, j in enumerate(lonlat)]
+                for sign in ['-', '+']]).replace(', [', ',[')
