db1960819f46f54345063324b4de99bff9894252
AmEl <amirelkanov0504@gmail.com>
2020-07-04 17:12:44 +0300

    Changed columns name

--- AmEl_Tourism/application/data/user_info.py
+++ AmEl_Tourism/application/data/user_info.py
@@ -8,15 +8,15 @@ class TGUserInfo(db.Model):
     """ TG User info model initialization class """
 
     __tablename__ = 'tg_user_info'
-    tg_user_id = db.Column(db.Integer, primary_key=True)
-    tg_user_coords = db.Column(db.String, nullable=True)
-    tg_user_page = db.Column(db.Integer, nullable=False, default=1)
+    id = db.Column(db.Integer, primary_key=True)
+    coords = db.Column(db.String, nullable=True)
+    page = db.Column(db.Integer, nullable=False, default=1)
 
 
 class VKUserInfo(db.Model):
     """ VK User info model initialization class """
 
     __tablename__ = 'vk_user_info'
-    vk_user_id = db.Column(db.Integer, primary_key=True)
-    vk_user_coords = db.Column(db.String, nullable=True)
-    vk_user_page = db.Column(db.Integer, nullable=False, default=1)
+    id = db.Column(db.Integer, primary_key=True)
+    coords = db.Column(db.String, nullable=True)
+    page = db.Column(db.Integer, nullable=False, default=1)
